b00ac945ce766ddc5f79b0ac708c697f657309bc
jiangzh <824041451@qq.com>
2016-12-28 18:09:35 +0800

    table尝试get json方法

--- blur-admin-master 12.20/blur-admin-master/src/app/pages/management/medalOperationList/medalOperationListCtrl.js
+++ blur-admin-master 12.20/blur-admin-master/src/app/pages/management/medalOperationList/medalOperationListCtrl.js
@@ -496,7 +496,6 @@
       }
     ];
 
-    $scope.editableTableData = $scope.smartTableData.slice(0, 36);
 
 
     $.each($scope.smartTableData,function(i){
@@ -508,41 +507,5 @@
         }
     });
 
-    $scope.showGroup = function(user) {
-      if(user.group && $scope.groups.length) {
-        var selected = $filter('filter')($scope.groups, {id: user.group});
-        return selected.length ? selected[0].text : 'Not set';
-      } else return 'Not set'
-    };
-
-    $scope.showStatus = function(user) {
-      var selected = [];
-      if(user.status) {
-        selected = $filter('filter')($scope.statuses, {value: user.status});
-      }
-      return selected.length ? selected[0].text : 'Not set';
-    };
-
-
-    $scope.removeUser = function(index) {
-      $scope.users.splice(index, 1);
-    };
-
-    $scope.addUser = function() {
-      $scope.inserted = {
-        id: $scope.users.length+1,
-        name: '',
-        status: null,
-        group: null
-      };
-      $scope.users.push($scope.inserted);
-    };
-
-    editableOptions.theme = 'bs3';
-    editableThemes['bs3'].submitTpl = '<button type="submit" class="btn btn-primary btn-with-icon"><i class="ion-checkmark-round"></i></button>';
-    editableThemes['bs3'].cancelTpl = '<button type="button" ng-click="$form.$cancel()" class="btn btn-default btn-with-icon"><i class="ion-close-round"></i></button>';
-
-
   }
-
 })();
